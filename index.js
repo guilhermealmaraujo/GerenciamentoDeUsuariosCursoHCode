5d590bf6c67d9e3a41e2424b81e6cf17cbdb1a50
Guilherme Araujo <guilherme.alm.araujo@gmail.com>
2020-11-22 00:24:33 -0300

    Adicionando evento click no botão salvar do formulário

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-var Name = document.querySelector("#exampleInputName")
-var gender = document.querySelector('#form-user-create [name=gender]:checked')
-var birth = document.querySelector('#exampleInputBirth')
-var country = document.querySelector('#exampleInputCountry')
-var email = document.querySelector('#exampleInputEmail')
-var password = document.querySelector('#exampleInputPassword')
-var photo = document.querySelector('#exampleInputFile')
-var admin = document.querySelector('#exampleInputAdmin')
-
 var fields = document.querySelectorAll("#form-user-create [name]")
 
-fields.forEach(function(field, index){
+var user = {}
 
-    if(field.name == "gender"){
-        
-        if(field.checked) {
-         
-            console.log("SIM!", field)
-        
+document.getElementById("form-user-create").addEventListener("submit", function(event){
+
+    event.preventDefault()
+
+    fields.forEach(function(field, index){
+
+        if(field.name == "gender"){
+            
+            if(field.checked) {
+             
+                user[field.name] = field.value
+            
+            }     
+            
+        }else{
+    
+            user[field.name] = field.value
+    
         }
+    
+    })
 
-        
-        
-    }else{
-
-        console.log("Não!!")
-
-    }
-
-    //console.log(field.id, field.value, field.name, field.checked, index) 
+    console.log(user)
 
 })
