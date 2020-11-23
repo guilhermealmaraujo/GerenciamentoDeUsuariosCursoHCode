c3767b00e534a4c250c7c48b03d6f79bf8496f8f
Guilherme Araujo <guilherme.alm.araujo@gmail.com>
2020-11-22 23:14:13 -0300

    Implementando OO ao passar as informações de usuário

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var user = {}
 
 function addLine(dataUser){
 
-    var tr = document.createElement("tr")
+    console.log(dataUser)
 
-    tr.innerHTML = `
+    document.getElementById("table-users").innerHTML = `
     <tr>
         <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
@@ -19,7 +19,6 @@ function addLine(dataUser){
         </td>
     </tr>`
 
-    document.getElementById("table-users").appendChild(tr)
 }
 
 document.getElementById("form-user-create").addEventListener("submit", function(event){
@@ -44,6 +43,16 @@ document.getElementById("form-user-create").addEventListener("submit", function(
     
     })
 
-    addLine(user)
+    var objectUser = new User(
+        user.name, 
+        user.gender, 
+        user.birth, user.country, 
+        user.email, 
+        user.password, 
+        user.photo, 
+        user.admin
+    )
+
+    addLine(objectUser)
 
 })
