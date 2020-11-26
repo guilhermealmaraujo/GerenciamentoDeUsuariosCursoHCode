f25f4918b2cc009c51d821af8e63193652809547
Guilherme Araujo <guilherme.alm.araujo@gmail.com>
2020-11-25 23:09:21 -0300

    Corrigindo bugs

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -25,7 +25,7 @@ class UserController{
                 (e)=>{
 
                     console.error(e)
-                    
+
                 }
             )
 
@@ -61,7 +61,10 @@ class UserController{
 
             }
 
-            fileReader.readAsDataURL(file)
+            if (file )
+                fileReader.readAsDataURL(file)
+            else
+                resolve('dist/img/boxed-bg.png')
 
         })
 
@@ -81,6 +84,10 @@ class UserController{
                 
                 }     
                 
+            }else if(field.name == "admin"){
+
+                user[field.name] = field.checked
+
             }else{
         
                 user[field.name] = field.value
@@ -104,18 +111,22 @@ class UserController{
 
     addLine(dataUser){
     
-        this.tableEl.innerHTML = `
+        let tr = document.createElement('tr')
+
+        tr.innerHTML = `
         <tr>
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
-            <td>${dataUser.admin}</td>
+            <td>${(dataUser.admin)? 'Sim' : 'Não'}</td>
             <td>${dataUser.birth}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
         </tr>`
+
+        this.tableEl.appendChild(tr) 
     
     }
 
